2017e16fa17f7a7229d8d10a2d34bdd270b4aa5e
AlexandrTarabanko <azerov.commerce@gmail.com>
2023-06-16 16:15:03 +0300

    SRC Back

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className="statistics">
+      {title && <h2 className="title">{title}</h2>}
+
+      <ul className="stat-list">
+        {stats.map(({ id, label, percentage }) => (
+          <li key={id} className="stat-item">
+            <span className="stat-label">{label} </span>
+            <span className="stat-percentage">{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.array.isRequired,
+};
